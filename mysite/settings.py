a804f55c3acde7b6b834d75a1a2fdbfcdce00be6
nilo <renard.agne@gmail.com>
2016-05-18 16:37:46 -0300

    mandando pro heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,9 +80,19 @@ MEDIA_URL = '/'
 #    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
 #    'Cache-Control': 'max-age=94608000',
 #}
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 STATIC_URL = '/static/'
 STATIC_ROOT='static'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
@@ -112,7 +122,9 @@ DATABASES = {
         
     }
 
-
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 ########################################
