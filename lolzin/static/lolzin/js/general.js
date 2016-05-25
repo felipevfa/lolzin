322a65269ff0f7b250ea49c909857c9b6d9ee07b
Felipe Aragão <felipevfaragao@gmail.com>
2016-05-25 10:53:36 -0300

    Fix

--- lolzin/static/lolzin/js/general.js
+++ lolzin/static/lolzin/js/general.js
@@ -3,19 +3,72 @@
  *  a manter a consistência das funcionalidades da aplicação. 
 */
 
+var adjustQuestionCardBackground = function () {
+	var cardHeight = 0;
+
+	cardHeight = $('.question-card').height();
+
+	$('.question-card::before').height(cardHeight);
+};
+
+var adjustSectionSize = function () {
+	var main = $("main");
+
+	console.log("OLAR");
+	main.has("section.section-wrapper").css("min-height", "calc(100vh - 106px)");
+};
+
+var setKeyboardShortcuts = function () {
+	$(document.body).on("keydown", function(e) {
+	
+		var keyNum = 49;
+
+		if (window.event) {
+			keyNum = e.keyCode;
+		}
+		else {
+			if (e.which) {
+				keyNum = e.which;
+			}
+		}
+
+		switch(keyNum) {
+			case 49:
+				$("#question-form #option-1").prop("checked", true);
+				break;
+			case 50:
+				$("#question-form #option-2").prop("checked", true);
+				break;
+			case 51:
+				$("#question-form #option-3").prop("checked", true);
+				break;
+			case 52:
+				$("#question-form #option-4").prop("checked", true);
+				break;
+			case 84:
+				$("#question-form input[value=1]").prop("checked", true);
+				break;
+
+			case 13:
+				var event = $.Event('click');
+				$("#answer-button").trigger(event);
+				break;
+
+			default:
+				break;
+		}
+	}); 
+};
+
 $(document).ready(function() {
-	var adjustQuestionCardBackground = function () {
-		var cardHeight = 0;
-
-		cardHeight = $('.question-card').height();
-
-		$('.question-card::before').height(cardHeight);
-	};
-
 	/* Inicialização da Navegação Mobile */
 	$('.button-collapse').sideNav();
 
-	$("main").has("section.section-wrapper").css("min-height", "calc(100vh - 106px)");
-
+	adjustSectionSize();
 	adjustQuestionCardBackground();
+	
+	if ($("#question-form").length > 0) {
+		console.log("OLAR");
+		setKeyboardShortcuts();
+	}
 });
